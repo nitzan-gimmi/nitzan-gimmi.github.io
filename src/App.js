717f6b351114300af9a8cb762ea3143c626b5e11
נצן בנין <nitzan.banin@gmail.com>
2025-10-18 18:33:07 +0300

    Add HFBT synergy React visualizer (src/)

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HFBSynergyDiagram from './HFBSynergyDiagram';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HFBT Synergy Visualizer</h1>
+      <HFBSynergyDiagram />
+    </div>
+  );
+}
+
+export default App;
